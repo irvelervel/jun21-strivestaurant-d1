2289b13bc8f5beef132830a523874a4f07163820
Stefano Casasola <ste.casasola@gmail.com>
2021-08-24 10:09:15 +0200

    First break

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// the first react component that gets its way into the DOM
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>I ERASED EVERYTHING!</div>
+      </header>
+    </div>
+  )
+}
+
+export default App
